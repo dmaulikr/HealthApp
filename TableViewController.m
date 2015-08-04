bf0785254263789bf05f56a7bf287a969c0ae6d5
Filipe Silva <FdSO@filipe-silva.local>
2015-08-04 10:54:09 -0400

    Alerta sobre Termo de Uso

--- TableViewController.m
+++ TableViewController.m
@@ -18,7 +18,17 @@
 {
     [super viewDidLoad];
     self.tableView.separatorColor = [UIColor clearColor];
+    UIAlertView *theAlert = [[UIAlertView alloc] initWithTitle:@"Termo de Uso" message:@"Este aplicativo serve apenas para fins informativos e não pretende ser um substituto de orientação, diagnóstico ou tratamento prestado por seu médico ou por outro profissional de saúde. Não nós responsabilizamos por o uso indevido de nossa ferramenta. Você aceita o Termo de Uso?" delegate:self cancelButtonTitle:@"Aceito" otherButtonTitles:@"Não Aceito", nil];
+    [theAlert show];
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger) buttonIndex
+{
     
+    if(buttonIndex != 0)
+    {
+        exit(0);
+    }
 }
 
 - (void)didReceiveMemoryWarning
